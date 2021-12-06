02dea74baaa85412b6ce446febad68dc7573c853
John Gozde <john@gozde.ca>
2021-12-05 23:34:08 -0700

    Ensmallen

--- day06/src/main.rs
+++ day06/src/main.rs
@@ -26,14 +26,10 @@ fn day6(mut v: impl Iterator<Item = String>, days: i64) -> i64 {
 
     for _ in 0..days {
         let fish_0 = fish[0];
-        fish[0] = fish[1];
-        fish[1] = fish[2];
-        fish[2] = fish[3];
-        fish[3] = fish[4];
-        fish[4] = fish[5];
-        fish[5] = fish[6];
-        fish[6] = fish[7] + fish_0;
-        fish[7] = fish[8];
+        for f in 0..8 {
+            fish[f] = fish[f + 1];
+        }
+        fish[6] += fish_0;
         fish[8] = fish_0;
     }
 
