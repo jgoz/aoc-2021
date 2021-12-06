fa7f05177a9c24e0cc21fc9ca9665bc73cb20230
John Gozde <john@gozde.ca>
2021-12-05 23:27:22 -0700

    Arrays exist

--- day06/src/main.rs
+++ day06/src/main.rs
@@ -1,4 +1,3 @@
-use std::collections::HashMap;
 use std::{env, io, io::prelude::*};
 
 fn main() {
@@ -25,31 +24,32 @@ fn day6(v: impl Iterator<Item = String>, days: i64) -> i64 {
         .map(|x| x.parse::<i32>().unwrap())
         .collect::<Vec<_>>();
 
-    let mut fish = HashMap::from([
-        (0, 0),
-        (1, initial.iter().filter(|x| **x == 1).count() as i64),
-        (2, initial.iter().filter(|x| **x == 2).count() as i64),
-        (3, initial.iter().filter(|x| **x == 3).count() as i64),
-        (4, initial.iter().filter(|x| **x == 4).count() as i64),
-        (5, initial.iter().filter(|x| **x == 5).count() as i64),
-        (6, initial.iter().filter(|x| **x == 6).count() as i64),
-        (7, 0),
-        (8, 0),
-    ]);
+    let mut fish = [
+        0,
+        initial.iter().filter(|x| **x == 1).count() as i64,
+        initial.iter().filter(|x| **x == 2).count() as i64,
+        initial.iter().filter(|x| **x == 3).count() as i64,
+        initial.iter().filter(|x| **x == 4).count() as i64,
+        initial.iter().filter(|x| **x == 5).count() as i64,
+        initial.iter().filter(|x| **x == 6).count() as i64,
+        0,
+        0,
+    ];
 
     for _ in 0..days {
-        let fish_0 = fish.insert(0, fish[&1]).unwrap_or(0);
-        fish.insert(1, fish[&2]);
-        fish.insert(2, fish[&3]);
-        fish.insert(3, fish[&4]);
-        fish.insert(4, fish[&5]);
-        fish.insert(5, fish[&6]);
-        fish.insert(6, fish[&7] + fish_0);
-        fish.insert(7, fish[&8]);
-        fish.insert(8, fish_0);
+        let fish_0 = fish[0];
+        fish[0] = fish[1];
+        fish[1] = fish[2];
+        fish[2] = fish[3];
+        fish[3] = fish[4];
+        fish[4] = fish[5];
+        fish[5] = fish[6];
+        fish[6] = fish[7] + fish_0;
+        fish[7] = fish[8];
+        fish[8] = fish_0;
     }
 
-    fish.into_iter().fold(0, |acc, (_, v)| acc + v)
+    fish.into_iter().fold(0, |acc, v| acc + v)
 }
 
 #[test]
